115e4ed7fd8bd618de2c5f33740b021851057be9
Jatin <js240899@gmail.com>
2021-12-19 13:12:54 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App = () => {
+
+    const [weight, setWeight] = useState("");
+    const [height, setHeight] = useState("");
+    const [bmiresult, setBmiresult] = useState();
+    const [status, setStatus] = useState("");
+
+
+    function calculate() {
+        let bmi = (weight / (height / 100) ** 2).toFixed(2);
+
+        setBmiresult(bmi);
+
+        if (bmi < 18.5) setStatus("UNDERWEIGHT");
+        else if (bmi >= 18.5 && bmi < 24.9) setStatus("NORMAL");
+        else if (bmi >= 25 && bmi < 29.9) setStatus("OVERWEIGHT");
+        else setStatus("OBESITY");
+    }
+    return ( <
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <
+        div className = 'App' >
+        <
+        h1 className = 'h1' > BMI Calculator < /h1> <
+        form className = "container" >
+        <
+        label
+        for = "height"
+        className = "height" > Height < /label> <
+        input type = "number"
+        placeholder = 'Height'
+        onChange = {
+            (e) => setHeight(e.target.value) }
+        /> <
+        br / >
+        <
+        label
+        for = "weight"
+        className = 'weight' > Weight < /label> <
+        input type = "number"
+        placeholder = 'Weight'
+        onChange = {
+            (e) => setWeight(e.target.value) }
+        /> <
+        br / >
+        <
+        button type = "button"
+        onClick = { calculate } > Calculate < /button> <
+        div className = 'incenter' >
+        <
+        h3 > Your consition is: { status } < /h3> <
+        h3 > Your BMI is: { bmiresult } < /h3> <
+        /div> <
+        /form>
+
+        <
+        /div>
+
+        <
+        />
+    )
 }
 
-export default App;
+export default App
