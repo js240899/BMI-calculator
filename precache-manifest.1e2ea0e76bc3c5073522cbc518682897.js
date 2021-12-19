1e5239efe5c9fc15895aa719670bb76fe5eaf71a
Jatin <js240899@gmail.com>
2021-12-19 19:25:45 +0530

    Updates

---
+++ precache-manifest.1e2ea0e76bc3c5073522cbc518682897.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ed2115f0e409f2226ea",
+    "url": "/BMI-calculator/static/css/main.3bae3b2d.chunk.css"
+  },
+  {
+    "revision": "9ed2115f0e409f2226ea",
+    "url": "/BMI-calculator/static/js/main.9ed2115f.chunk.js"
+  },
+  {
+    "revision": "a3c0bc647fa7f281b58f",
+    "url": "/BMI-calculator/static/js/1.a3c0bc64.chunk.js"
+  },
+  {
+    "revision": "4d0e6887d99b58842ae9",
+    "url": "/BMI-calculator/static/js/runtime~main.4d0e6887.js"
+  },
+  {
+    "revision": "d05c18f262b440798c756207e220eecb",
+    "url": "/BMI-calculator/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/BMI-calculator/precache-manifest.1e2ea0e76bc3c5073522cbc518682897.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/BMI-calculator/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
